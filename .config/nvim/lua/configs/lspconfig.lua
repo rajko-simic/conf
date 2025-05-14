12361abf5ecb3a12c35757b0ddb8ec252bd6ac63
rajko <rajko@mingle.com>
2025-05-14 09:16:18 +0200

    Add gitted nvim config

---
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,27 @@
+require("nvchad.configs.lspconfig").defaults()
+
+local servers = {
+  "bashls",
+  "bufls",
+  "dartls",
+  "dockerls",
+  "gopls",
+  "graphql",
+  "groovyls",
+  "jsonls",
+  "kotlin_language_server",
+  "lua_ls",
+  "marksman",
+  "nginx_language_server",
+  "omnisharp",
+  "pyright",
+  "rust_analyzer",
+  "sqlls",
+  "terraformls",
+  "vimls",
+  "yamlls",
+}
+
+vim.lsp.enable(servers)
+
+-- read :h vim.lsp.config for changing options of lsp servers 
