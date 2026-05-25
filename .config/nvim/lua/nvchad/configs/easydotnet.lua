dd3afdbafd1ae21568174e7e16cce4a1eccd076a
Rajko Simic <rajko.simic@centralsquare.com>
2026-05-25 14:20:27 +0000

    dotnet solution scan

--- .config/nvim/lua/nvchad/configs/easydotnet.lua
+++ .config/nvim/lua/nvchad/configs/easydotnet.lua
@@ -2,5 +2,13 @@ require("easy-dotnet").setup {
   lsp = {
     enabled = true,
     roslynator_enabled = true,
+    config = {
+      settings = {
+        ["csharp|background_analysis"] = {
+          dotnet_analyzer_diagnostics_scope = "openFiles",
+          dotnet_compiler_diagnostics_scope = "fullSolution",
+        },
+      },
+    },
   },
 }
